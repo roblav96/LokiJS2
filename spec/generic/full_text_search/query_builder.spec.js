c0a8852b795672483a0996ee8e4ee5fb2657234b
Toni Neubert <lutztonineubert@gmail.com>
2017-02-04 16:45:03 +0100

    Update some documentations of inverted index queries.

---
+++ spec/generic/full_text_search/query_builder.spec.js
@@ -0,0 +1,13 @@
+/* global describe, it, expect */
+import {QueryBuilder} from '../../../src/inverted_index/queries';
+
+describe('tokenizer', function () {
+
+	it('splitter', function (done) {
+		let tkz = new Tokenizer();
+
+
+
+		done();
+	});
+});
