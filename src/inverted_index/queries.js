5c647f7d2592513dedcae3293003fbe2a3eb6628
Toni Neubert <lutztonineubert@gmail.com>
2017-02-04 21:38:39 +0100

    Update documentation for inverted index queries.

--- src/inverted_index/queries.js
+++ src/inverted_index/queries.js
@@ -1,10 +1,13 @@
 /**
  * Query builder
+ * todo: Document scoring.
+ * todo: Align description.
  */
 
 /**
- * @param {string} field - the field name of the document where the term should be searched
- * @param {string} term - the term to search
+ * The base query class to enable boost to a query type.
+ *
+ * @param {string} type - the type name of the query
  */
 export class BaseQuery {
 	constructor(type, data = {}) {
@@ -14,6 +17,10 @@ export class BaseQuery {
 
 	/**
 	 * Boosts the query result.
+	 *
+	 * See also [Lucene#BoostQuery]{@link https://lucene.apache.org/core/6_4_0/core/org/apache/lucene/search/BoostQuery.html}
+	 * and [Elasticsearch#boost]{@link https://www.elastic.co/guide/en/elasticsearch/reference/5.2/mapping-boost.html}.
+	 *
 	 * @param {number} value - the positive boost
 	 * @return {BaseQuery} object itself for cascading
 	 */
@@ -33,12 +40,21 @@ export class BaseQuery {
 		return this._data;
 	}
 }
+
 /**
+ * A query which finds documents where a document field contains a term.
  *
- * Hello world, abc
+ * See also [Lucene#TermQuery]{@link https://lucene.apache.org/core/6_4_0/core/org/apache/lucene/search/TermQuery.html}
+ * and [Elasticsearch#TermQuery]{@link https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-term-query.html}.
  *
- * @param {string} field - the field name of the document where the term should be searched
- * @param {string} term - the term to search
+ * @example
+ * new TermQuery("name", "infinity"])
+ * .build();
+ * // The resulting documents:
+ * // contains the term infinity
+ *
+ * @param {string} field - the field name of the document
+ * @param {string} term - the term
  * @extends BaseQuery
  */
 export class TermQuery extends BaseQuery {
@@ -50,28 +66,55 @@ export class TermQuery extends BaseQuery {
 }
 
 /**
-
- * @param {string} field - the field name of the document where the term should be searched
- * @param {string[]} terms - the terms to search
+ * A query which finds documents where a document field contains any of the terms.
+ *
+ * See also [Lucene#TermRangeQuery]{@link https://lucene.apache.org/core/6_4_0/core/org/apache/lucene/search/TermRangeQuery.html}
+ * and [Elasticsearch#TermsQuery]{@link https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-terms-query.html}.
+ *
+ * @example
+ * new TermsQuery("quotes", ["infinity", "atom", "energy"])
+ * .build();
+ * // The resulting documents:
+ * // contains the terms infinity, atom or energy
+ *
+ * @param {string} field - the field name of the document
+ * @param {string[]} terms - the terms
  * @extends BaseQuery
  */
 export class TermsQuery extends BaseQuery {
 	constructor(field, terms, data = {}) {
 		super("terms", data);
-		this._data.field = field;
 		if (!Array.isArray(terms)) {
 			throw TypeError("Value for terms must be an array.");
 		}
+		this._data.field = field;
 		this._data.values = terms;
 	}
 }
 
 /**
- * Single character wildcard is the ? (question mark).
- * Multiple character wildcard is the * (asterisk).
+ * A query which finds documents where the wildcard term can be applied at an existing document field term.
  *
- * @param {string} field - the field name of the document where the term should be searched
- * @param {string} wildcard - the wildcard to search
+ * Wildcard | Description
+ * -------- | ------------
+ * ? (question mark) | Skips a single character.
+ *
+ * To escape a wildcard character, use _\_ (backslash), e.g. \?.
+ *
+ * See also [Lucene#WildcardQuery]{@link https://lucene.apache.org/core/6_4_0/core/org/apache/lucene/search/WildcardQuery.html}
+ * and [Elasticsearch#WildcardQuery]{@link https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-wildcard-query.html}.
+ *
+ * _TODO: Implement wildcard * (asterisk) to skip zero or more characters._
+ * @todo Implement wildcard * (asterisk) to skip zero or more characters.
+ *
+ * @example
+ * new WildcardQuery("question", "e?nste?n\?")
+ * .build();
+ * // The resulting documents:
+ * // contains the wildcard surname e?nste?n\? (like Einstein? or Eynsteyn? but not Einsteine or Ensten?)
+ *
+ * @param {string} field - the field name of the document
+ * @param {string} wildcard - the wildcard term
  * @extends BaseQuery
  */
 export class WildcardQuery extends BaseQuery {
@@ -83,8 +126,29 @@ export class WildcardQuery extends BaseQuery {
 }
 
 /**
- * @param {string} field - the field name of the document where the term should be searched
- * @param {string} fuzzy - the fuzzy to search
+ * A query which finds documents where the fuzzy term can be transformed into an existing document field term within a
+ * given edit distance
+ * ([Damerau–Levenshtein distance]{@link https://en.wikipedia.org/wiki/Damerau%E2%80%93Levenshtein_distance}).
+ *
+ * The edit distance is the minimum number of an insertion, deletion or substitution of a single character
+ * or a transposition of two adjacent characters.
+ *
+ * * To set the maximal allowed edit distance, use {@link FuzzyQuery#fuzziness} (default is 2).
+ * * To set the initial word length, which should ignored for fuzziness, use {@link FuzzyQuery#prefixLength}.
+ *
+ * See also [Lucene#FuzzyQuery]{@link https://lucene.apache.org/core/6_4_0/core/org/apache/lucene/search/FuzzyQuery.html}
+ * and [Elasticsearch#FuzzyQuery]{@link https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-fuzzy-query.html}.
+ *
+ * @example
+ * new FuzzyQuery("surname", "einsten")
+ * 	.fuzziness(3)
+ * 	.prefixLength(3)
+ * .build();
+ * // The resulting documents:
+ * // contains the fuzzy surname einstn (like Einstein or Einst but not Eisstein or Insten)
+ *
+ * @param {string} field - the field name of the document
+ * @param {string} fuzzy - the fuzzy term
  * @extends BaseQuery
  */
 export class FuzzyQuery extends BaseQuery {
@@ -108,7 +172,7 @@ export class FuzzyQuery extends BaseQuery {
 	}
 
 	/**
-	 * Sets the starting word length which should not be considered for fuzzy query.
+	 * Sets the initial word length.
 	 * @param {number} prefixLength - the positive prefix length
 	 * @return {FuzzyQuery}  object itself for cascading
 	 */
@@ -122,8 +186,20 @@ export class FuzzyQuery extends BaseQuery {
 }
 
 /**
- * @param {string} field - the field name of the document where the term should be searched
- * @param {string} prefix - the positive prefix length
+ * A query which matches documents containing the prefix of a term inside a field.
+ *
+ * See also [Lucene#PrefixQuery]{@link https://lucene.apache.org/core/6_4_0/core/org/apache/lucene/search/PrefixQuery.html}
+ * and [Elasticsearch#MatchQuery]{@link https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-prefix-query.html}
+ *
+ * @example
+ * new QueryBuilder()
+ *   .prefix("surname", "alb")
+ * .build()
+ * // The resulting documents:
+ * // contains the term prefix alb as surname
+ *
+ * @param {string} field - the field name of the document
+ * @param {string} prefix - the prefix of a term
  * @extends BaseQuery
  */
 export class PrefixQuery extends BaseQuery {
@@ -135,7 +211,18 @@ export class PrefixQuery extends BaseQuery {
 }
 
 /**
- * @param {string} field - the field name of the document where the term should be searched
+ * A query which matches all documents with a given field.
+ *
+ * @example
+ * new QueryBuilder()
+ *   .exists("name")
+ * .build()
+ * // The resulting documents:
+ * // has the field "name"
+ *
+ * See also [Lucene#?]{@link ?} and [Elasticsearch#ExistsQuery]{@link https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-exists-query.html}.
+ *
+ * @param {string} field - the field name of the document
  * @extends BaseQuery
  */
 export class ExistsQuery extends BaseQuery {
@@ -146,8 +233,31 @@ export class ExistsQuery extends BaseQuery {
 }
 
 /**
- * @param {string} field - the field name of the document where the term should be searched
- * @param {string} query - the field name of the document where the term should be searched
+ * A query which tokenizes the given query text, performs a query foreach token and combines the results using a boolean
+ * operator.
+ *
+ * Operator      | Description
+ * ------------- | -------------
+ * or (default) | Finds documents which matches some tokens. The minimum amount of matches can be controlled with [minimumShouldMatch]{@link MatchQuery#minimumShouldMatch} (default is 1).
+ * and | Finds documents which matches all tokens.
+ *
+ * To enable a [fuzzy query]{@link FuzzyQuery} for the tokens, use {@link MatchQuery#fuzziness} and {@link MatchQuery#prefixLength}.
+ *
+ * See also [Lucene#?]{@link ?}
+ * and [Elasticsearch#MatchQuery]{@link https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-match-query.html}.
+ *
+ * @example
+ * new MatchQuery("name", "albrt einsten")
+ *   .boost(2.5)
+ *   .operator("and")
+ *   .fuzziness(2)
+ *   .prefixLength(3)
+ * .build();
+ * // The resulting documents:
+ * // contains the fuzzy name albrt einsten (like Albert Einstein) with a boost of 2.5
+ *
+ * @param {string} field - the field name of the document
+ * @param {string} query - the query text
  * @extends BaseQuery
  */
 export class MatchQuery extends BaseQuery {
@@ -158,8 +268,8 @@ export class MatchQuery extends BaseQuery {
 	}
 
 	/**
-	 * Sets m
-	 * @param {number} minShouldMatch
+	 * Controls the amount of minimum matching token queries before a document will be considered.
+	 * @param {number} minShouldMatch - number of minimum matching sub queries
 	 * @return {MatchQuery} object itself for cascading
 	 */
 	minimumShouldMatch(minShouldMatch) {
@@ -174,18 +284,18 @@ export class MatchQuery extends BaseQuery {
 	}
 
 	/**
-	 *
-	 * @param value
+	 * Sets the boolean operator.
+	 * @param {string} op - the operator (_or_/_and_)
 	 * @return {MatchQuery} object itself for cascading
 	 */
-	operator(value) {
-		if (typeof(value) !== "string") {
+	operator(op) {
+		if (typeof(op) !== "string") {
 			throw TypeError("Value for operator must be a string.");
 		}
-		if (value != 'and' && value != 'or') {
+		if (op != 'and' && op != 'or') {
 			throw SyntaxError("Unknown operator.");
 		}
-		this._data.operator = value;
+		this._data.operator = op;
 		if (this._data.hasOwnProperty("minimum_should_match") && this._data.operator == "and") {
 			throw SyntaxError("Match query with \"and\" operator does not support minimum should match.");
 		}
@@ -193,8 +303,8 @@ export class MatchQuery extends BaseQuery {
 	}
 
 	/**
-	 *
-	 * @param fuzziness
+	 * Sets the maximal allowed fuzziness.
+	 * @param {number} fuzziness - the fuzziness
 	 * @return {MatchQuery} object itself for cascading
 	 */
 	fuzziness(fuzziness) {
@@ -206,9 +316,9 @@ export class MatchQuery extends BaseQuery {
 	}
 
 	/**
-	 *
-	 * @param prefixLength
-	 * @return {InvertedIndex.Queries.MatchQuery} - object itself for cascading
+	 * Sets the starting word length which should not be considered for fuzziness.
+	 * @param {number} prefixLength - the positive prefix length
+	 * @return {MatchQuery} - object itself for cascading
 	 */
 	prefixLength(prefixLength) {
 		if (typeof(prefixLength) !== "number" || prefixLength < 0) {
@@ -225,11 +335,15 @@ export class MatchQuery extends BaseQuery {
  * Typically used inside a must clause of a {@link BoolQuery} to subsequently reject non matching documents with the not
  * clause.
  *
+ * See also [Lucene#MatchAllDocsQuery]{@link https://lucene.apache.org/core/6_4_0/core/org/apache/lucene/search/MatchAllDocsQuery.html}
+ * and [Elasticsearch#MatchAllQuery]{@link https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-match-all-query.html}.
+ *
  * @example
- * new QueryBuilder()
- *   .matchAll().boost(2.5)
+ * new MatchAll()
+ *   .boost(2.5)
  * .build()
- * // Matches all documents and giving them a score of 2.5.
+ * // The resulting documents:
+ * // all documents and giving a score of 2.5
  *
  * @extends BaseQuery
  */
@@ -315,13 +429,16 @@ class ArrayQuery extends BaseQuery {
 /**
  * A query that wraps sub queries and returns a constant score equal to the query boost for every document in the filter.
  *
+ * See also [Lucene#BooleanQuery]{@link https://lucene.apache.org/core/6_4_0/core/org/apache/lucene/search/ConstantScoreQuery.html}
+ * and [Elasticsearch#ConstantScoreQuery]{@link https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-constant-score-query.html}.
+ *
  * @example
- * new QueryBuilder()
- *   .constantScore().boost(1.5)
- *     .startFilter()
- *       .term("first_name", "albert")
- *       .term("surname", "einstein")
- *     .endFilter()
+ * new ConstantScore()
+ *   .boost(1.5)
+ *   .startFilter()
+ *     .term("first_name", "albert")
+ *     .term("surname", "einstein")
+ *   .endFilter()
  * .build()
  * // The resulting documents:
  * // * contains albert as first name, einstein as surname and the document score is 42.
@@ -350,33 +467,35 @@ export class ConstantScoreQuery extends BaseQuery {
  *
  * Occur         | Description
  * ------------- | -------------
- * must  | Only documents where all sub queries match will be considered and contribute to the score.
- * filter  | Only documents where all sub queries match will be considered but not contribute to the score.
- * should  | Only documents where some sub queries match will be considered and contribute to the score. The minimum amount of matches can be controlled with [minimumShouldMatch]{@link BoolQuery#minimumShouldMatch}.
+ * must  | Finds documents which matches all sub queries.
+ * filter  | Finds documents which matches all sub queries but these documents do not contribute to the score.
+ * should  | Finds documents which matches some sub queries. The minimum amount of matches can be controlled with [minimumShouldMatch]{@link BoolQuery#minimumShouldMatch} (default is 1).
  * not  | Documents which match any sub query will be ignored.
  *
  * A sub query can be any other query type and also the bool query itself.
  *
+ * See also [Lucene#BooleanQuery]{@link https://lucene.apache.org/core/6_4_0/core/org/apache/lucene/search/BooleanQuery.html}
+ * and [Elasticsearch#BoolQuery]{@link https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-bool-query.html}.
+ *
  * @example
- * new QueryBuilder()
- *   .bool()
- *     .startMust()
- *       .term("first_name", "albert")
- *     .endMust()
- *     .startFilter()
- *       .term("birthplace", "ulm")
- *     .endFilter()
- *     .startShould().minimumShouldMatch(2)
- *       .fuzzy("surname", "einstin")
- *       .wildcard("name", "geni?s")
- *       .term("quotes", "infinity")
- *     .endShould()
- *     .startNot()
- *       .terms("research_field", ["biology", "geography"])
- *     .endNot()
+ * new BoolQuery()
+ *   .startMust().boost(2)
+ *     .term("first_name", "albert")
+ *   .endMust()
+ *   .startFilter()
+ *     .term("birthplace", "ulm")
+ *   .endFilter()
+ *   .startShould().minimumShouldMatch(2)
+ *     .fuzzy("surname", "einstin")
+ *     .wildcard("name", "geni?s")
+ *     .term("quotes", "infinity")
+ *   .endShould()
+ *   .startNot()
+ *     .terms("research_field", ["biology", "geography"])
+ *   .endNot()
  * .build();
  * // The resulting documents:
- * // contains the name albert (must: contribute to the score)
+ * // contains the name albert (must: contribute to the score with a boost of 2)
  * // contains the birthplace ulm (filter: not contribute to the score)
  * // contains a minimum of two matches from the fuzzy, wildcard and/or term query (should: contribute to the score)
  * // do not contains biology or geography as research field (not: not contribute to the score)
@@ -386,11 +505,6 @@ export class ConstantScoreQuery extends BaseQuery {
 export class BoolQuery extends BaseQuery {
 	constructor(data = {}) {
 		super("bool", data);
-
-		let self = this;
-		this._callback = () => {
-			return self;
-		};
 	}
 
 	/**
@@ -399,7 +513,7 @@ export class BoolQuery extends BaseQuery {
 	 */
 	startMust() {
 		this._data.must = {};
-		return new ArrayQuery("endMust", this._callback, this._data.must);
+		return new ArrayQuery("endMust", () => {return this;}, this._data.must);
 	}
 
 	/**
@@ -408,7 +522,7 @@ export class BoolQuery extends BaseQuery {
 	 */
 	startFilter() {
 		this._data.filter = {};
-		return new ArrayQuery("endFilter", this._callback, this._data.filter);
+		return new ArrayQuery("endFilter", () => {return this;}, this._data.filter);
 	}
 
 	/**
@@ -417,7 +531,7 @@ export class BoolQuery extends BaseQuery {
 	 */
 	startShould() {
 		this._data.should = {};
-		return new ArrayQuery("endShould", this._callback, this._data.should);
+		return new ArrayQuery("endShould", () => {return this;}, this._data.should);
 	}
 
 	/**
@@ -426,7 +540,7 @@ export class BoolQuery extends BaseQuery {
 	 */
 	startNot() {
 		this._data.not = {};
-		return new ArrayQuery("endNot", this._callback, this._data.not);
+		return new ArrayQuery("endNot", () => {return this;}, this._data.not);
 	}
 
 	/**
@@ -449,17 +563,6 @@ export class BoolQuery extends BaseQuery {
 export class QueryBuilder {
 	constructor() {
 		this._data = {query: {}};
-
-		let self = this;
-		let callback = () => {
-			return self._data;
-		};
-
-		this._prepare = (queryType, ...args) => {
-			this._child = new queryType(...args, this._data.query);
-			this._child.build = callback;
-			return this._child;
-		};
 	}
 
 	enableFinalScoring(enabled) {
@@ -524,5 +627,11 @@ export class QueryBuilder {
 
 	exists(field) {
 		return this._prepare(ExistsQuery, field);
+	}
+
+	_prepare(queryType, ...args) {
+		this._child = new queryType(...args, this._data.query);
+		this._child.build = () => {return this._data};
+		return this._child;
 	}
 }
