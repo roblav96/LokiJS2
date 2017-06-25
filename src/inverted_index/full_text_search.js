df47e08c69f7d95194c9ab6d2a353c4765942586
Viatorus <lutztonineubert@gmail.com>
2017-06-25 12:26:00 +0200

    Make fts document id field configurable.

--- src/inverted_index/full_text_search.js
+++ src/inverted_index/full_text_search.js
@@ -3,21 +3,22 @@ import {IndexSearcher} from './index_searcher';
 import {Tokenizer} from './tokenizer';
 
 export class FullTextSearch {
-	/**
-	 * Initialize the full text search for the given fields.
-	 * @param {object[]} fields - the field options
-	 * @param {string} fields.name - the name of the field
-	 * @param {boolean=true} fields.store - flag to indicate if the full text search should be stored on serialization or
-	 *	rebuild on deserialization
-	 *@param {Tokenizer=Tokenizer} fields.tokenizer - the tokenizer of the field
-	 */
-  constructor(fields) {
+  /**
+   * Initialize the full text search for the given fields.
+   * @param {object[]} fields - the field options
+   * @param {string} fields.name - the name of the field
+   * @param {boolean=true} fields.store - flag to indicate if the full text search should be stored on serialization or
+   *  rebuild on deserialization
+   * @param {Tokenizer=Tokenizer} fields.tokenizer - the tokenizer of the field
+   * @param {string=$loki} id - the field name of the document index
+   */
+  constructor(fields, {id = "$loki"} = {}) {
     if (fields === undefined) {
       throw new SyntaxError('Fields needs to be defined!');
     }
 
     this._invIdxs = {};
-		// Get field names and tokenizers.
+    // Get field names and tokenizers.
     if (Array.isArray(fields)) {
       for (let i = 0; i < fields.length; i++) {
         let field = fields[i];
@@ -36,38 +37,38 @@ export class FullTextSearch {
     } else {
       throw new TypeError('fields needs to be an array with field name and a tokenizer (optional).');
     }
-
+    this._id = id;
     this._docs = new Set();
     this._idxSearcher = new IndexSearcher(this._invIdxs, this._docs);
   }
 
   addDocument(doc) {
-    if (doc.$loki === undefined) {
+    if (doc[this._id] === undefined) {
       throw new Error('Document is not stored in the collection.');
     }
 
     let fieldNames = Object.keys(doc);
     for (let i = 0, fieldName; i < fieldNames.length, fieldName = fieldNames[i]; i++) {
       if (this._invIdxs[fieldName] !== undefined) {
-        this._invIdxs[fieldName].insert(doc[fieldName], doc.$loki);
+        this._invIdxs[fieldName].insert(doc[fieldName], doc[this._id]);
       }
     }
 
-    this._docs.add(doc.$loki);
+    this._docs.add(doc[this._id]);
     this.setDirty();
   }
 
   removeDocument(doc) {
-    if (doc.$loki === undefined) {
+    if (doc[this._id] === undefined) {
       throw new Error('Document is not stored in the collection.');
     }
 
     let fieldNames = Object.keys(this._invIdxs);
     for (let i = 0; i < fieldNames.length; i++) {
-      this._invIdxs[fieldNames[i]].remove(doc.$loki);
+      this._invIdxs[fieldNames[i]].remove(doc[this._id]);
     }
 
-    this._docs.delete(doc.$loki);
+    this._docs.delete(doc[this._id]);
     this.setDirty();
   }
 
